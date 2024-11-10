f1236e61a313fca52b4a3e33850dcedac19a138b
saehuihwang <saehui.hwang@gmail.com>
2024-11-09 16:33:07 -0800

    random destination coordinate

--- Assets/Scripts/Helpers.cs
+++ Assets/Scripts/Helpers.cs
@@ -1,16 +1,16 @@
-public static class Helpers
-{
-    public static float CalculateDistance(Vector3 pointA, Vector3 pointB)
-    {
-        return Vector3.Distance(pointA, pointB);
-    }
-    public static Vector3 GetRandomDestination(Vector3 minBounds, Vector3 maxBounds, string areaType)
-    {
-        // Generate random coordinates within the specified bounds
-        float randomX = Random.Range(minBounds.x, maxBounds.x);
-        float randomY = Random.Range(minBounds.y, maxBounds.y);
-        float randomZ = Random.Range(minBounds.z, maxBounds.z);
+// public static class Helpers
+// {
+//     public static float CalculateDistance(Vector3 pointA, Vector3 pointB)
+//     {
+//         return Vector3.Distance(pointA, pointB);
+//     }
+//     public static Vector3 GetRandomDestination(Vector3 minBounds, Vector3 maxBounds, string areaType)
+//     {
+//         // Generate random coordinates within the specified bounds
+//         float randomX = Random.Range(minBounds.x, maxBounds.x);
+//         float randomY = Random.Range(minBounds.y, maxBounds.y);
+//         float randomZ = Random.Range(minBounds.z, maxBounds.z);
 
-        return new Vector3(randomX, randomY, randomZ);
-    }
-}
+//         return new Vector3(randomX, randomY, randomZ);
+//     }
+// }
